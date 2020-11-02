988493e2e1be9ba8a5cb21bba4b3ae5742921230
Lea Dalibard <leadalibard@gmail.com>
2020-11-02 14:57:12 +0100

    Adding class Course

--- Model/Course.php
+++ Model/Course.php
@@ -2,6 +2,54 @@
 
 
 class Course
-{
+{/** @var array Student[] */
+
+ private array $students;
+ private int $id;
+ private string $location;
+ private string $name;
+ private string $teacherName;
+
+
+    public function __construct(array $students, $id, $location, $name, $teacherName)
+    {
+        $this->students = $students;
+        $this->id = $id;
+        $this->location = $location;
+        $this->name = $name;
+        $this->teacherName = $teacherName;
+    }
+
+
+    public function getStudents()
+    {
+        return $this->students;
+    }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public function getTeacherName()
+    {
+        return $this->teacherName;
+    }
+
+
 
 }
