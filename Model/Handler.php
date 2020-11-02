c7042408304cd3a41b954317e716d5c90cf94754
Lea Dalibard <leadalibard@gmail.com>
2020-11-02 16:08:07 +0100

    Adding a getStudents function for particular teacher + update teacher

--- Model/Handler.php
+++ Model/Handler.php
@@ -68,4 +68,36 @@ class Handler
         $handle->bindValue(':classes_id', $classes_id);
         $handle->execute();
     }
+
+    public function updateTeacher($id,$name, $email, $classeName)
+    {
+        $classes = $this->pdo->getClasses();
+        foreach ($classes as $course) {
+            if ($course['name'] == $classeName) {
+                $classes_id = $course['id'];
+            }
+        }
+        $handle = $this->pdo->prepare('UPDATE teachers SET name =:name, email =:email, classes_id = :classes_id WHERE id = :id');
+        $handle->bindValue(':id', $id);
+        $handle->bindValue(':name', $name);
+        $handle->bindValue(':email', $email);
+        $handle->bindValue(':classes_id', $classes_id);
+        $handle->execute();
+    }
+
+    function getStudents($teacherCourse)
+    {
+        $students = $this->pdo->getStudents();
+        $teacherStudents = array();
+        foreach ($students as $student) {
+            if ($student['class_id'] == $teacherCourse) {
+                array_push($teacherStudents, $student);
+            }
+        }
+        return $teacherStudents;
+    }
+
+
+
+
 }
--- Model/Teacher.php
+++ Model/Teacher.php
@@ -4,31 +4,5 @@
 class Teacher extends Student
 {
 
-    function getStudents($teacherCourse)
-    {
-        $students = $this->pdo->getStudents();
-        $teacherStudents = array();
-        foreach ($students as $student) {
-            if ($student['class_id'] == $teacherCourse) {
-                array_push($teacherStudents, $student);
-            }
-        }
-        return $teacherStudents;
 
-    }
-
-    public function updateTeacher($id,$name, $email, $classeName)
-    {
-        $classes = $this->pdo->getClasses();
-        foreach ($classes as $course) {
-            if ($course['name'] == $classeName) {
-                $classes_id = $course['id'];
-            }
-        }
-            $handle = $this->pdo->prepare('UPDATE teachers SET name =:name, email =:email, classes_id = :classes_id WHERE id = :id');
-            $handle->bindValue(':id', $id);
-            $handle->bindValue(':name', $name);
-            $handle->bindValue(':email', $email);
-            $handle->bindValue(':classes_id', $classes_id);
-            $handle->execute();
-    }
+}
