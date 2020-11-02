f175f3ba8d3267ef33985074b4295c9c92a13a7b
Akenaftw <felix.j.c.vanroost@gmail.com>
2020-11-02 14:00:27 +0100

    added the config file and updated the handler class

--- Model/Handler.php
+++ Model/Handler.php
@@ -4,27 +4,27 @@
 class Handler
 {
     private PDO $pdo;
-    private PDOStatement $handle;
 
 
     public function __construct()
     {
         $this->pdo = $this->openConnection();
+
     }
 
     public function openConnection(): PDO
     {
-        $dbhost = "localhost";
-        $dbuser = "becode";
-        $dbpass = "Becode123!";
-        $db = "php-crud";
-
-        $driverOptions = [
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ];
+        require 'config.php';
 
         return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
     }
+
+
+    public function getStudents()
+    {
+        $handle = $this->pdo->prepare('SELECT * FROM students');
+        $handle->execute();
+        return $handle->fetchAll();
+    }
+
 }
