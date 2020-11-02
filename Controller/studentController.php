9e27c780a73c7f14b602c583417bb276eb3887e2
Akenaftw <felix.j.c.vanroost@gmail.com>
2020-11-02 16:41:56 +0100

    fixed small bugg where teachercontroller was class

--- Controller/studentController.php
+++ Controller/studentController.php
@@ -1,7 +1,2 @@
 <?php
-<<<<<<< HEAD
-require "View/studentView.php";
-=======
 
-
->>>>>>> c7042408304cd3a41b954317e716d5c90cf94754
--- Controller/teacherController.php
+++ Controller/teacherController.php
@@ -1,22 +1,2 @@
 <?php
-<<<<<<< HEAD
-require "View/teacherView.php";
-=======
-
-class TeacherController
-{
-    public function render(array $GET, array $POST)
-
-    {
-
-//-----> get teachers
-        $pdo = new Handler();
-        $Teachers=$pdo->getTeachers();
-
-
-
-
-    }
-
-}
->>>>>>> c7042408304cd3a41b954317e716d5c90cf94754
+require "View/studentView.php";
