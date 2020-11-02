00de7dae898c119669378844b5e7490331a91183
Fib88 <nsukifrank@hotmail.com>
2020-11-02 15:42:18 +0100

    save state before pull

--- Model/Student.php
+++ Model/Student.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class Student 
+class Student
 {
     private string $name;
     private string $email;
@@ -22,7 +22,7 @@ class Student
         $this->id = $id;
         $this->classId = $classId;
     }
-
+    
     /**
      * @return string
      */
