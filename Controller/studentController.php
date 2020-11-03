f2cb3b2c2b1e209533a77fea274b4149b7e3fa91
Fib88 <nsukifrank@hotmail.com>
2020-11-03 16:22:59 +0100

    fixing merge conflict

--- Controller/studentController.php
+++ Controller/studentController.php
@@ -4,16 +4,6 @@
     $showStudents = new Handler();
     $students = $showStudents->getStudents();
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 0123a8e01b6e7ce0dbad6c3af1ced5ecd6ed3f8d
-
-
-
-
-
 require "View/studentView.php";
 
 
