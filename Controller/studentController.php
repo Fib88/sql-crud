1f89f380578370b3fd619c7fb8efa34559727aef
Akenaftw <felix.j.c.vanroost@gmail.com>
2020-11-03 15:59:43 +0100

    added pages for view

--- Controller/studentController.php
+++ Controller/studentController.php
@@ -1,10 +1,9 @@
 <?php
 
 //if button on overview is clicked load up and show all students
-if(isset($_POST["students"])){
     $showStudents = new Handler();
     $students = $showStudents->getStudents();
-}
+
 
 
 
