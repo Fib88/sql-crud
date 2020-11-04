0bb23d65fffacc6e13f51fd2a120e32481b60230
Fib88 <nsukifrank@hotmail.com>
2020-11-04 12:01:03 +0100

    made teachers show up from class on classview

--- View/courseView.php
+++ View/courseView.php
@@ -7,9 +7,8 @@
     <tr>
         <td>
             <a href="?page=classes&course=<?php echo $course["id"]; ?>"class="text-dark"><?php echo $course["name"]; ?></a>
-        </td>
-        <td>
-            <?php echo $course["location"]; ?>
+
+            <?php echo " in ". " ".$course["location"]; ?>
         </td>
     </tr>
     <?php endforeach; ?>
@@ -17,7 +16,13 @@
     </tr>
 </table>
 
+
+
 <?php if (isset($_GET['course'])): ?>
+    <h1>Teacher</h1>
+
+    <?php echo $teacherShow['name']?>
+
     <h2>Students</h2>
     <table class="table table-striped table-wide text-dark">
         <thead>
